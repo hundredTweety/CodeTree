d6b9d684f48f7fe9a6aa9b4425879f597cc4853b
hundredTweety <154863073+hundredTweety@users.noreply.github.com>
2026-09-07 15:27:42 +0900

    [Passed] 남녀노소 구분짓기 (48ms, 16MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+S = int(input())
+A = int(input())
+
+if S == 0 and A >=19:
+    print("MAN")
+
+elif S==0 and A <19:
+    print("BOY")
+
+elif S==1 and A >=19:
+    print("WOMAN")
+
+else:
+    print("GIRL")
