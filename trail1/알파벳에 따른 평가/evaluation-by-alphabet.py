743c7c673a75ecb3d9350ac0f2fbc0c1316cdcd1
hundredTweety <154863073+hundredTweety@users.noreply.github.com>
2026-09-07 14:17:16 +0900

    [Passed] 알파벳에 따른 평가 (48ms, 16MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,19 @@
+upper = str(input())
+
+if upper == "S":
+    print("Superior")
+
+elif upper == "A":
+    print("Excellent")
+
+elif upper == "B" :
+    print("Good")
+
+elif upper == "C":
+    print("Usually")
+
+elif upper == "D":
+    print("Effort")
+
+else:
+    print("Failure")
