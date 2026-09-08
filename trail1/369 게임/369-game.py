0e229bda23f235d586007d97f945b2131dd34a9b
hundredTweety <154863073+hundredTweety@users.noreply.github.com>
2026-09-08 11:38:52 +0900

    [Passed] 369 게임 (47ms, 16MB)

--- trail1/369 게임/369-game.py
+++ trail1/369 게임/369-game.py
@@ -1,7 +1,7 @@
 N = int(input())
 i=1
 while i <= N:
-    if i %3 ==0 or i%6 == 0 or i%9 == 0:
+    if i %3 ==0:
         print(0, end = " ")
     elif '3' in str(i) or '6' in str(i)or '9' in str(i):
         print(0, end = " ")
