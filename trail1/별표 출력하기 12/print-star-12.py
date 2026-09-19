432a7bc97b2b85750a8627e4d2503ebfd243cc56
hundredTweety <154863073+hundredTweety@users.noreply.github.com>
2026-09-19 18:15:55 +0900

    [Passed] 별표 출력하기 12 (48ms, 16MB)

---
+++ trail1/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N):
+    for j in range(N):
+
+        if i == 0:
+            print("*", end=" ")
+
+        elif j % 2 != 0 and j >= i:
+            print("*", end=" ")
+
+        else:
+            print(" ", end=" ")
+
+    print()
